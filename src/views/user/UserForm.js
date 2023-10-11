7a7f2476bc915fb284cae80a63ca9712b960c028
thiagodalsanto <thiagodalsantodaluz@gmail.com>
2023-10-11 15:18:03 -0300

    navbar

--- src/views/user/UserForm.js
+++ src/views/user/UserForm.js
@@ -2,26 +2,29 @@
 export default function UserForm() {
     return (
         <>
-            <h2 className="text-gray-500 text-lg text-center">Usuário</h2>
 
-            <div>
-                <div>
-                <label class="block text-gray-700 text-sm font-bold mb-2" for="nome">Nome</label>
-                <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" placeholder="Nome"/>
-                </div>
+            <div className="containe w-96 h-128 bg-gray-200 rounded-xl shadow border p-8 m-10 content-center">
+                <h2 className="text-gray-500 text-lg text-center">Usuário</h2>
 
                 <div>
-                    <label class="block text-gray-700 text-sm font-bold mb-2" for="Email">Email</label>
-                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" placeholder="Email"/>
-                </div>
+                    <div>
+                    <label class="block text-gray-700 text-sm font-bold mb-2" for="nome">Nome</label>
+                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" placeholder="Nome"/>
+                    </div>
 
-                <div>
-                    <label class="block text-gray-700 text-sm font-bold mb-2" for="senha">Senha</label>
-                    <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" placeholder="Senha"/>
-                </div>
+                    <div>
+                        <label class="block text-gray-700 text-sm font-bold mb-2" for="Email">Email</label>
+                        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" placeholder="Email"/>
+                    </div>
 
-                <div class="py-3">
-                    <button type="submit" class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">Salvar</button>
+                    <div>
+                        <label class="block text-gray-700 text-sm font-bold mb-2" for="senha">Senha</label>
+                        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="text" placeholder="Senha"/>
+                    </div>
+
+                    <div class="py-8">
+                        <button type="submit" class="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">Salvar</button>
+                    </div>
                 </div>
             </div>
         </>
--- src/views/user/UserList.js
+++ src/views/user/UserList.js
@@ -1,12 +1,17 @@
 import axios from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { API_URL } from "../../AppConsts";
 
 export default function UserList() {
+
+    const [users, setUsers] = useState([]);
+
     useEffect( () => {
         axios.get(API_URL + '/users/')
             .then(res => {
                 console.log(res);
+                if( res.status === 200)
+                    setUsers(res.data.data);
             })
             .catch(err => {
                 console.error(err);
@@ -15,24 +20,29 @@ export default function UserList() {
 
     return (
         <>
-            <h2>Usuários</h2>
-            <table className="table">
-                <thead>
-                    <tr>
-                        <th></th>
-                        <th>Nome</th>
-                        <th>Email</th>
-                    </tr>
-                </thead>
+            <div className="containe mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10 content-center">
+                <h2 className="text-center py-8">Usuários</h2>
+                <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                    <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                        <tr>
+                            <th scope="col" class="px-6 py-3"></th>
+                            <th scope="col" class="px-6 py-3">Nome</th>
+                            <th scope="col" class="px-6 py-3">Email</th>
+                        </tr>
+                    </thead>
 
-                <tbody>
-                    <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                </tbody>
-            </table>
+                    <tbody>
+                        { users.map ( u => (
+                            <tr key={u.id} class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                <td class="py-4 px-4"><img src={u.avatar} alt={u.last_name}/></td>
+                                <td class="py-4">{u.first_name} {u.last_name}</td>
+                                <td class="py-4">{u.email}</td>
+                            </tr>
+                        ))
+                        }
+                    </tbody>
+                </table>
+            </div>
         </>
     );
 }
