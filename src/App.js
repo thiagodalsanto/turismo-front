88d2727be75c3675a145da7f990b2ce21e5a3f8f
Thiago Dal Santo <thiagodalsantodaluz@gmail.com>
2023-11-25 09:25:46 -0300

    link activating in routing

--- src/App.js
+++ src/App.js
@@ -2,12 +2,32 @@ import './App.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import AppRoutes from './AppRouter';
 import NavBar from './components/NavBar.js';
+import { useEffect, useState } from "react";
 
 export default function App() {
+
+  // let [darkMode, setDarkMode] = useState(false);
+
+  let [darkMode, setDarkMode] = useState(() => {
+    const storedDarkMode = localStorage.getItem("darkMode");
+
+    return storedDarkMode ? JSON.parse(storedDarkMode) : false;
+  });
+
+  useEffect(() => {
+      if (darkMode) {
+          document.documentElement.classList.add("dark");
+      } else {
+          document.documentElement.classList.remove("dark");
+      }
+
+      localStorage.setItem('darkMode', JSON.stringify(darkMode));
+  }, [darkMode])
+
   return (
     <>
-      <NavBar/>
       <Router >
+        <NavBar darkMode={darkMode} setDarkMode={setDarkMode}/>
         <AppRoutes/>
       </Router>
     </>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,18 +1,7 @@
-import { useEffect } from "react";
-import { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
 
-export default function NavBar() {
-
-    let [darkMode, setDarkMode] = useState(false);
-
-    useEffect(() => {
-        if (darkMode) {
-            document.documentElement.classList.add("dark");
-        } else {
-            document.documentElement.classList.remove("dark");
-        }
-    }, [darkMode])
-
+export default function NavBar({ darkMode, setDarkMode }) {
+    const location = useLocation();
 
     return (
         <nav class="bg-white border-gray-200 dark:bg-gray-900">
@@ -22,13 +11,13 @@ export default function NavBar() {
                 <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">EROTURISM</span>
             </a>
             <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-                <button onClick={() => { setDarkMode(!darkMode); }} className="flex justify-center items-center mr-2 rounded-lg bg-fuchsia-600 hover:bg-fuchsia-800 w-9 h-9">
-                    { darkMode ? <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="white" class="bi bi-sun" viewBox="0 0 16 16"> <path d="M8 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6zm0 1a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z"/></svg> : <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="white" class="bi bi-moon" viewBox="0 0 16 16">
+                <button onClick={() => { setDarkMode(!darkMode); }} className="flex justify-center items-center mr-2 rounded-lg bg-gray-300 hover:bg-gray-400 dark:hover:bg-gray-600 dark:bg-gray-700 w-9 h-9">
+                    { darkMode ? <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="white" class="bi bi-sun" viewBox="0 0 16 16"> <path d="M8 11a3 3 0 1 1 0-6 3 3 0 0 1 0 6zm0 1a4 4 0 1 0 0-8 4 4 0 0 0 0 8zM8 0a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 0zm0 13a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 13zm8-5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2a.5.5 0 0 1 .5.5zM3 8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1 0-1h2A.5.5 0 0 1 3 8zm10.657-5.657a.5.5 0 0 1 0 .707l-1.414 1.415a.5.5 0 1 1-.707-.708l1.414-1.414a.5.5 0 0 1 .707 0zm-9.193 9.193a.5.5 0 0 1 0 .707L3.05 13.657a.5.5 0 0 1-.707-.707l1.414-1.414a.5.5 0 0 1 .707 0zm9.193 2.121a.5.5 0 0 1-.707 0l-1.414-1.414a.5.5 0 0 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .707zM4.464 4.465a.5.5 0 0 1-.707 0L2.343 3.05a.5.5 0 1 1 .707-.707l1.414 1.414a.5.5 0 0 1 0 .708z"/></svg> : <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="black" class="bi bi-moon" viewBox="0 0 16 16">
   <path d="M6 .278a.768.768 0 0 1 .08.858 7.208 7.208 0 0 0-.878 3.46c0 4.021 3.278 7.277 7.318 7.277.527 0 1.04-.055 1.533-.16a.787.787 0 0 1 .81.316.733.733 0 0 1-.031.893A8.349 8.349 0 0 1 8.344 16C3.734 16 0 12.286 0 7.71 0 4.266 2.114 1.312 5.124.06A.752.752 0 0 1 6 .278M4.858 1.311A7.269 7.269 0 0 0 1.025 7.71c0 4.02 3.279 7.276 7.319 7.276a7.316 7.316 0 0 0 5.205-2.162c-.337.042-.68.063-1.029.063-4.61 0-8.343-3.714-8.343-8.29 0-1.167.242-2.278.681-3.286z"/>
 </svg> }
                 </button>
                 <a href="/user/login">
-                    <button type="button" class="text-white bg-fuchsia-600 hover:bg-fuchsia-800 focus:ring-4 focus:outline-none focus:ring-fuchsia-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-fuchsia-600 dark:hover:bg-fuchsia-700 dark:focus:ring-fuchsia-800">Sign In</button>
+                    <button type="button" class="text-black dark:text-white bg-gray-300 hover:bg-gray-400 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-fuchsia-800">Sign In</button>
                 </a>
                 <button data-collapse-toggle="navbar-cta" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-cta" aria-expanded="false">
                 <span class="sr-only">Main Menu</span>
@@ -39,15 +28,21 @@ export default function NavBar() {
             </div>
             <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-cta">
                 <ul class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-                <li>
-                    <a href="/" class="block py-2 px-3 md:p-0 text-white bg-fuchsia-700 rounded md:bg-transparent md:text-fuchsia-700 md:dark:text-fuchsia-500" aria-current="page">Home</a>
-                </li>
-                <li>
-                    <a href="/tour" class="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-fuchsia-700 md:dark:hover:text-fuchsia-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Tours</a>
-                </li>
-                <li>
-                    <a href="/" class="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-fuchsia-700 d:dark:hover:text-fuchsia-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</a>
-                </li>
+                    <li>
+                        <Link to="/" className={`block py-2 px-3 md:p-0 rounded dark:text-gray-300 ${location.pathname === '/' ? 'text-fuchsia-600 dark:text-fuchsia-600 hover:text-fuchsia-500	dark:hover:text-fuchsia-700' : 'text-gray-700 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-300'}`} aria-current={location.pathname === '/' && 'page'}>
+                            Home
+                        </Link>
+                    </li>
+                    <li>
+                    <Link to="/tour" className={`block py-2 px-3 md:p-0 rounded dark:text-gray-300 ${location.pathname === '/tour' ? 'text-fuchsia-600 dark:text-fuchsia-600 hover:text-fuchsia-500	dark:hover:text-fuchsia-700' : 'text-gray-700 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-300'}`}>
+                        Tours
+                    </Link>
+                    </li>
+                    <li>
+                        <Link to="/" className={`block py-2 px-3 md:p-0 rounded dark:text-gray-300 ${location.pathname === '/' ? 'text-fuchsia-600 dark:text-fuchsia-600 hover:text-fuchsia-500	dark:hover:text-fuchsia-700' : 'text-gray-700 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-300'}`}>
+                            Services
+                        </Link>
+                    </li>
                 </ul>
             </div>
             </div>
