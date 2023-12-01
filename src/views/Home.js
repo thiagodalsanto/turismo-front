79c52e84d4f8b67b0287ea3aed450c6b5ca3dad7
thiago dal santo <thiagodalsantodaluz@gmail.com>
2023-12-01 14:37:33 -0300

    .

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,12 +1,16 @@
 
 export default function Home() {
     return (
-        <section class="bg-gray-50 dark:bg-gray-900" style={{overflow: "hidden", width: "100vw", height: "calc(100vh - 72px)"}}>
-            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-                <a href="/" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-                    <img class="w-24 h-24 mr-12" src="/images/hot-face-svgrepo-com.svg" alt="logo"/>
-                    <span class="text-8xl">EROTURISM</span>    
-                </a>
+        <section class="bg-gray-50 dark:bg-gray-900 flex flex-row justify-center items-center" style={{overflow: "hidden", width: "100vw", height: "calc(100vh - 72px)"}}>
+            <div class="mr-48">
+                <h2 class="text-9xl font-mono font-thin italic text-amber-600 drop-shadow-md">Beer</h2>
+                <h2 class="text-9xl font-mono font-thin italic text-amber-600 drop-shadow-md">Tourism</h2>
+                <p class="text-xl text-justify font-mono max-w-lg text-gray-500 drop-shadow-md mt-8">
+                Explore destinos cervejeiros com o Beer Tourism. Encontre pacotes de viagem, descubra pubs autênticos e experiências de degustação únicas. Planeje sua jornada cervejeira agora!
+                </p>
+            </div>
+            <div>
+                <img src="/images/home.png" class="h-5/6" alt="Logo" />
             </div>
         </section>
     );
